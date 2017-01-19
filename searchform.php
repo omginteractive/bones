002681f2c4f2e055a60a8bfe04cdb2ec1f6f7812
Julio Vedovatto <juliovedovatto@gmail.com>
2017-01-19 19:40:21 -0200

    changed search form structure to act like bootstrap

--- searchform.php
+++ searchform.php
@@ -1,8 +1,8 @@
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url( '/' ); ?>">
-    <div>
-        <label for="s" class="screen-reader-text"><?php _e('Search for:','bonestheme'); ?></label>
-        <input type="search" id="s" name="s" value="" />
+<form role="search" method="get" id="searchform" class="form-inline searchform" action="<?php echo home_url( '/' ); ?>">
+    <div class="form-group">
+        <label for="s" class="sr-only screen-reader-text"><?php _e('Search for:','bonestheme'); ?></label>
+        <input type="search" id="s" name="s" value="" class="form-control" placeholder="<?php _e('Search for:','bonestheme'); ?>" />
 
-        <button type="submit" id="searchsubmit" ><?php _e('Search','bonestheme'); ?></button>
+        <button type="submit" id="searchsubmit" class="btn btn-default"><?php _e('Search','bonestheme'); ?></button>
     </div>
 </form>
